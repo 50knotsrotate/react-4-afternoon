6348bb13270de44be863e91f6528dd676e72eeb3
= <=>
2019-04-11 20:11:29 -0700

    react router dom

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount = () => {
+const className = this.props.match.params.class
+    axios.get(`http://localhost:3005/students?class=${className}`)
+      .then(res => {
+        this.setState({
+          students: res.data
+        })
+    })
+
   }
 
   render() {
+    const classTitle = this.props.match.params.class
+    const students = this.state.students.map((student, i) => { 
+      return (
+        <div key={i}>
+          <Link to={`/${classTitle}/${student.id}`}><h3>{student.first_name}</h3></Link> 
+        </div>
+      )
+    })
     return (
       <div className="box">
-        <h1></h1>
-        <h2>ClassList:</h2>
+        <h1>{classTitle}</h1>
+        <h2>ClassList: {students}</h2>
 
       </div>
     )
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Student extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state = {
+      student: {}
+    }
 
   }
 
+  componentDidMount = () => { 
+    const { id } = this.props.match.params
+    const className = this.props.match.params.class
+    axios.get(`http://localhost:3005/students?class=${className}`)
+      .then(res => {
+        this.setState({
+          student: res.data.filter(student => student.id == id )[0]
+        })
+      })
+      
+    }
+
   render() {
+    const student = (
+      <div>
+        <h1>{this.state.student.first_name} {this.state.student.last_name}</h1>
+        <h2>Grade: {this.state.student.grade}</h2>
+        <h2>Email: {this.state.student.email}</h2>
+      </div>
+    )
     return (
       <div className="box">
-        <h1>Student</h1>
+        <h1>Student: {this.state.student.first_name ? this.state.student.first_name : null}</h1>
+        {this.state.student.first_name ? student : <h1>Loading...</h1>}
       </div>
     )
   }
